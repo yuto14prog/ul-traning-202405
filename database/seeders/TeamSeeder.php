97b87014af3cfa30ed239599befcb4c5ac69e3b6
yuto14prog <yuto14prog@gmail.com>
2024-05-07 08:19:42 +0000

    TeamのSeederファイルを記述

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TeamSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class TeamSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('teams')->insert([
+            'name' => 'test1',
+            'owner_id' => '1',
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'test2',
+            'owner_id' => '2',
+        ]);
     }
 }
