4f3e661c95cb23893de6afe0c1bec4861ccef54d
yuto14prog <yuto14prog@gmail.com>
2024-05-07 08:33:20 +0000

    ディレクトリの移動でエラー出たのを解消

--- app/Http/Controllers/manager/TeamController.php
+++ app/Http/Controllers/manager/TeamController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\manager;
+use App\Http\Controllers\Controller;
 
 use App\Models\Team;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\TeamController;
+use App\Http\Controllers\manager\TeamController;
 
 /*
 |--------------------------------------------------------------------------
