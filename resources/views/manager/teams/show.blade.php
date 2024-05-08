d830f6bd7d99e21f772b8e0cb678f6df30e14ded
yuto14prog <yuto14prog@gmail.com>
2024-05-08 08:21:31 +0000

    インデントが漏れていたので修正

--- resources/views/manager/teams/show.blade.php
+++ resources/views/manager/teams/show.blade.php
@@ -3,7 +3,7 @@
     <h2>{{ $team->name }}(id:{{ $team->id }})</h2>
     <div class="mb-3">
         <div class="text-end mb-2">
-        <a href="{{ route('manager.teams.edit', $team) }}" class="btn btn-primary">編集</a>
+            <a href="{{ route('manager.teams.edit', $team) }}" class="btn btn-primary">編集</a>
         </div>  
     </div>
 </x-layout>
