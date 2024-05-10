b528a60a411276dfec24141502ef8448183da88b
yuto14prog <yuto14prog@gmail.com>
2024-05-10 08:03:09 +0000

    Bladeの細かいことを修正

--- resources/views/manager/tasks/create.blade.php
+++ resources/views/manager/tasks/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot name="title">Task作成</x-slot>
-    <h2><a href="{{ route('manager.teams.show', $team) }}">{{$team->name}}</a>/タスク新規作成</h2>
+    <h2><a href="{{ route('manager.teams.show', $team) }}">{{ $team->name }}</a>/タスク新規作成</h2>
     <x-form-error />
     <x-mini-panel>
         <form action="{{ route('manager.teams.tasks.store', $team) }}" method="post">
@@ -8,7 +8,7 @@
 
             <div class="mb-3">
                 <label class="form-label" for="title">タイトル</label>
-                <input type="text" name="title" value="{{old('title')}}" id="title"
+                <input type="text" name="title" value="{{ old('title') }}" id="title"
                     class="form-control @error('title') is-invalid @enderror">
                 @error('title')
                     <div class="invalid-feedback">{{ $message }}</div>
@@ -16,7 +16,7 @@
             </div>
             <div class="mb-3">
                 <label class="form-label" for="body">内容</label>
-                <textarea type="text" name="body" value="{{old('body')}}" id="body"
+                <textarea type="text" name="body" value="{{ old('body') }}" id="body"
                     class="form-control @error('body') is-invalid @enderror"></textarea>
                 @error('body')
                     <div class="invalid-feedback">{{ $message }}</div>
--- resources/views/manager/tasks/edit.blade.php
+++ resources/views/manager/tasks/edit.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
     <x-slot name="title">Task/edit</x-slot>
-    <h2><a href="{{ route('manager.teams.show', $team) }}">{{$team->name}}</a>/{{ $task->title }}編集</h2>
+    <h2><a href="{{ route('manager.teams.show', $team) }}">{{ $team->name }}</a>/{{ $task->title }}編集</h2>
+    <x-form-error />
     <x-mini-panel>
-        <x-form-error />
         <form action="{{ route('manager.teams.tasks.update', [$team, $task]) }}" method="post">
             @csrf
             @method('PATCH')
 
             <div class="mb-3">
                 <label class="form-label" for="title">タイトル</label>
-                <input type="text" name="title" value="{{$task->title}}" id="title"
+                <input type="text" name="title" value="{{ $task->title }}" id="title"
                     class="form-control @error('title') is-invalid @enderror">
                 @error('title')
                     <div class="invalid-feedback">{{ $message }}</div>
@@ -18,7 +18,7 @@
             <div class="mb-3">
                 <label class="form-label" for="body">内容</label>
                 <textarea type="text" name="body" value="" id="body"
-                    class="form-control @error('body') is-invalid @enderror">{{$task->body}}</textarea>
+                    class="form-control @error('body') is-invalid @enderror">{{ $task->body }}</textarea>
                 @error('body')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
