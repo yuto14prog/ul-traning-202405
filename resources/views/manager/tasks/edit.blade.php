9d58d9aab33c8afa51af922fcf35e43949977372
yuto14prog <yuto14prog@gmail.com>
2024-05-13 00:54:03 +0000

    Review: old()を追加

--- resources/views/manager/tasks/edit.blade.php
+++ resources/views/manager/tasks/edit.blade.php
@@ -9,7 +9,7 @@
 
             <div class="mb-3">
                 <label class="form-label" for="title">タイトル</label>
-                <input type="text" name="title" value="{{ $task->title }}" id="title"
+                <input type="text" name="title" value="{{ old('title', $task->title) }}" id="title"
                     class="form-control @error('title') is-invalid @enderror">
                 @error('title')
                     <div class="invalid-feedback">{{ $message }}</div>
@@ -18,7 +18,7 @@
             <div class="mb-3">
                 <label class="form-label" for="body">内容</label>
                 <textarea type="text" name="body" value="" id="body"
-                    class="form-control @error('body') is-invalid @enderror">{{ $task->body }}</textarea>
+                    class="form-control @error('body') is-invalid @enderror">{{ old('title', $task->body) }}</textarea>
                 @error('body')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
