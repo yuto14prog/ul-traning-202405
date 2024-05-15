6d48a2766adb4d6fe943cf87fff89b93e9ba4e22
yuto14prog <yuto14prog@gmail.com>
2024-05-15 07:13:43 +0000

    TeamTestファイルを作成

---
+++ tests/Unit/TeamTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TeamTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
