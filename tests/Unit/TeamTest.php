09473e295f9112832f83957107f35c6aea0b42a4
yuto14prog <yuto14prog@gmail.com>
2024-05-16 02:09:19 +0000

    Review:: カウントのテストを追加＆Teamの名前をチェック

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Member;
 use App\Models\Team;
 use App\Models\User;
 use Tests\TestCase;
@@ -15,6 +16,9 @@ class TeamTest extends TestCase
      */
     public function test_createWithOwner()
     {
+        $team_count = Team::count();
+        $member_count = Member::count();
+
         $data = ['name' => 'teamname'];
         $user = User::factory()->create();
 
@@ -22,10 +26,13 @@ class TeamTest extends TestCase
         $member = $team->members()->where('user_id', $user->id)->first();
 
         // Teams
+        $this->assertEquals($team_count + 1, Team::count());
         $this->assertTrue($team->exists);
+        $this->assertEquals($team->name, $data['name']);
         $this->assertEquals($team->owner_id, $user->id);
 
         // Members
+        $this->assertEquals($member_count + 1, Member::count());
         $this->assertTrue($member->exists);
         $this->assertEquals($member->user_id, $user->id);
         $this->assertEquals($member->role, 1);
