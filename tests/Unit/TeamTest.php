86acc5d7232cb4a3d0d3a038ece14d333380807b
yuto14prog <yuto14prog@gmail.com>
2024-05-16 01:31:59 +0000

    Testの記述を変更

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -18,15 +18,16 @@ class TeamTest extends TestCase
         $data = ['name' => 'teamname'];
         $user = User::factory()->create();
 
-        $resultTeam = Team::createWithOwner($user, $data);
-        $resultMember = $resultTeam->members()->where('user_id', $user->id)->first();
+        $team = Team::createWithOwner($user, $data);
+        $member = $team->members()->where('user_id', $user->id)->first();
 
         // Teams
-        $this->assertDatabaseHas('teams', ['name' => $data['name']]);
-        $this->assertEquals($resultTeam->owner_id, $user->id);
+        $this->assertTrue($team->exists);
+        $this->assertEquals($team->owner_id, $user->id);
 
         // Members
-        $this->assertDatabaseHas('members', ['user_id' => $user->id]);
-        $this->assertEquals($resultMember->role, 1);
+        $this->assertTrue($member->exists);
+        $this->assertEquals($member->user_id, $user->id);
+        $this->assertEquals($member->role, 1);
     }
 }
