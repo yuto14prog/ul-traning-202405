b18497458b534cdc655d84e176baf523f05caab3
yuto14prog <yuto14prog@gmail.com>
2024-05-16 09:32:53 +0000

    wipコミット

--- tests/Feature/Me/TaskTest.php
+++ tests/Feature/Me/TaskTest.php
@@ -2,21 +2,46 @@
 
 namespace Tests\Feature\Me;
 
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_api_me_tasks()
     {
-        $response = $this->get('/');
+        $user = User::factory()->make();
+        Sanctum::actingAs($user);
 
-        $response->assertStatus(200);
+        $task1 = new Task([
+            'title' => 'title1',
+            'body' => 'body1',
+            'status' => 0,
+            'assignee_id' => $user->id,
+        ]);
+        $task1->team_id = 1;
+        $task1->save();
+        $task2 = new Task([
+            'title' => 'title2',
+            'body' => 'body2',
+            'status' => 0,
+            'assignee_id' => $user->id,
+        ]);
+        $task2->team_id = 1;
+        $task2->save();
+
+        $response = $this->getJson(route('api.me.index'));
+
+        $response->assertOk();
+        $response->assertJsonCount(2);
     }
 }
