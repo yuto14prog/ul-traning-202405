34a06c1b8c25e1a1c017230d024952be8dcd379b
yuto14prog <yuto14prog@gmail.com>
2024-05-17 06:28:17 +0000

    実装

--- app/Http/Controllers/Api/Me/TeamController.php
+++ app/Http/Controllers/Api/Me/TeamController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Api\Me;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TeamController extends Controller
 {
     public function index()
     {
-        return response()->json();
+        $user = Auth::user();
+        return response()->json($user->teams);
     }
 }
