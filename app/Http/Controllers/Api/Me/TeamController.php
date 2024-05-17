e0be25a9834921bedb53244808693606c1a7831e
yuto14prog <yuto14prog@gmail.com>
2024-05-17 06:19:10 +0000

    実装

---
+++ app/Http/Controllers/Api/Me/TeamController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\Me;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+    public function index()
+    {
+        return response()->json();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\Me\TaskController as MeTaskController;
+use App\Http\Controllers\Api\Me\TeamController as MeTeamController;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\UserController;
 
@@ -34,4 +35,5 @@ Route::middleware(['auth:sanctum'])
     ->group(function () {
         Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('task');
         Route::get('/me/tasks', [MeTaskController::class, 'index'])->name('me.tasks');
+        Route::get('/me/teams', [MeTeamController::class, 'index'])->name('me.teams');
 });
