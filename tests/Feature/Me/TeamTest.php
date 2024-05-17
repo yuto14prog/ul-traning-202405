08a66657b5c366086087dd6312dec453341585ca
yuto14prog <yuto14prog@gmail.com>
2024-05-17 06:18:36 +0000

    Test（apiからJsonを受け取れるまで）

--- tests/Feature/Me/TeamTest.php
+++ tests/Feature/Me/TeamTest.php
@@ -2,21 +2,27 @@
 
 namespace Tests\Feature\Me;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class TeamTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_api_me_teams()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
 
-        $response->assertStatus(200);
+        $response = $this->getJson(route('api.me.teams'));
+
+        $response->assertOk();
     }
 }
