270fb6b8188dadb0c9d9f3b7ca1017696e2b6085
yuto14prog <yuto14prog@gmail.com>
2024-05-17 06:28:08 +0000

    Test（Userに関連するTeam１つがJsonで受け取れる）

--- tests/Feature/Me/TeamTest.php
+++ tests/Feature/Me/TeamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Me;
 
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -21,8 +22,11 @@ class TeamTest extends TestCase
         $user = User::factory()->create();
         Sanctum::actingAs($user);
 
+        $team1 = Team::createWithOwner($user, ['name' => 'team1']);
+
         $response = $this->getJson(route('api.me.teams'));
 
         $response->assertOk();
+        $response->assertJsonCount(1);
     }
 }
