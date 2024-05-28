ab56318aaf6b016b9a94799b00867f1f6639a1ec
yuto14prog <yuto14prog@gmail.com>
2024-05-28 08:36:52 +0000

    Review:: <table>を修正

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,30 +2,42 @@
   <div class="home">
     <h2>アサインされているタスク</h2>
     <table>
-        <th>チーム</th>
-        <th>タスクID</th>
-        <th>タイトル</th>
-        <th>担当者</th>
-        <th>作成日時</th>
-        <th>操作</th>
-        <tr v-for="task in tasks" :key="task.id">
-            <td>{{ task.team_id }}</td>
-            <td>{{ task.id }}</td>
-            <td>{{ task.title }}</td>
-            <td>{{ task.assignee_id }}</td>
-            <td>{{ task.created_at }}</td>
-            <td>dummy</td>
-        </tr>
+        <thead>
+            <tr>
+                <th>チーム</th>
+                <th>タスクID</th>
+                <th>タイトル</th>
+                <th>担当者</th>
+                <th>作成日時</th>
+                <th>操作</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="task in tasks" :key="task.id">
+                <td>{{ task.team_id }}</td>
+                <td>{{ task.id }}</td>
+                <td>{{ task.title }}</td>
+                <td>{{ task.assignee_id }}</td>
+                <td>{{ task.created_at }}</td>
+                <td>dummy</td>
+            </tr>
+        </tbody>
     </table>
 
     <h2>所属しているチーム</h2>
     <table>
-        <th>チームID</th>
-        <th>チーム名</th>
-        <tr v-for="(team, index) in teams" :key="index">
-            <td>{{ team.id }}</td>
-            <td>{{ team.name }}</td>
-        </tr>
+        <thead>
+            <tr>
+                <th>チームID</th>
+                <th>チーム名</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(team, index) in teams" :key="index">
+                <td>{{ team.id }}</td>
+                <td>{{ team.name }}</td>
+            </tr>
+        </tbody>
     </table>
   </div>
 </template>
