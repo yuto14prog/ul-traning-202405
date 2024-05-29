3c68f4250803baf68ba6504bd11a012b66193eb8
yuto14prog <yuto14prog@gmail.com>
2024-05-29 02:52:04 +0000

    Squashed merge issue11

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,30 +2,42 @@
   <div class="home">
     <h2>アサインされているタスク</h2>
     <table>
-        <th>チーム</th>
-        <th>タスクID</th>
-        <th>タイトル</th>
-        <th>担当者</th>
-        <th>作成日時</th>
-        <th>操作</th>
-        <tr v-for="(task, index) in tasks" :key="index">
-            <td>{{ task.team_id }}</td>
-            <td>{{ task.id }}</td>
-            <td>{{ task.title }}</td>
-            <td>{{ task.assignee_id }}</td>
-            <td>{{ task.created_at }}</td>
-            <td><router-link :to="{ name: 'task', params: { id: task.id } }">詳細</router-link></td>
-        </tr>
+        <thead>
+            <tr>
+                <th>チーム</th>
+                <th>タスクID</th>
+                <th>タイトル</th>
+                <th>担当者</th>
+                <th>作成日時</th>
+                <th>操作</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="task in tasks" :key="task.id">
+                <td>{{ task.team_id }}</td>
+                <td>{{ task.id }}</td>
+                <td>{{ task.title }}</td>
+                <td>{{ task.assignee_id }}</td>
+                <td>{{ task.created_at }}</td>
+                <td>dummy</td>
+            </tr>
+        </tbody>
     </table>
 
     <h2>所属しているチーム</h2>
     <table>
-        <th>チームID</th>
-        <th>チーム名</th>
-        <tr v-for="(team, index) in teams" :key="index">
-            <td>{{ team.id }}</td>
-            <td>{{ team.name }}</td>
-        </tr>
+        <thead>
+            <tr>
+                <th>チームID</th>
+                <th>チーム名</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(team, index) in teams" :key="index">
+                <td>{{ team.id }}</td>
+                <td>{{ team.name }}</td>
+            </tr>
+        </tbody>
     </table>
   </div>
 </template>
@@ -49,17 +61,22 @@ import { onMounted, ref } from 'vue';
 export default {
   name: 'HomeView',
   setup() {
-    let tasks = ref({})
-    let teams = ref({})
+    let tasks = ref([])
+    let teams = ref([])
 
     onMounted(async function() {
-        const tasksUrl = '/api/me/tasks'
-        const tasksRes = await axios.get(tasksUrl)
-        tasks.value = tasksRes.data
+        const fetchTasks = async function() {
+            const tasksUrl = '/api/me/tasks'
+            const tasksRes = await axios.get(tasksUrl)
+            tasks.value = tasksRes.data
+        }
+        const fetchTeams = async function() {
+            const teamsUrl = '/api/me/teams'
+            const teamsRes = await axios.get(teamsUrl)
+            teams.value = teamsRes.data
+        }
 
-        const teamsUrl = '/api/me/teams'
-        const teamsRes = await axios.get(teamsUrl)
-        teams.value = teamsRes.data
+        await Promise.all([fetchTasks(), fetchTeams()])
     })
 
     return {
