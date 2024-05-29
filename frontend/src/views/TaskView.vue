77477bc5d57b4892b21730db7ca66e6a01c5ac53
yuto14prog <yuto14prog@gmail.com>
2024-05-29 04:03:15 +0000

    Apiでデータ取れるまで

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -1,12 +1,27 @@
 <template>
-    <span>task</span>
+    <h2></h2>
 </template>
 
 <script>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router'
 
 export default {
+    name: 'TaskView',
     setup() {
-        
+        const route = useRoute()
+        let task = ref([])
+
+        onMounted(async function() {
+            const url = `/api/tasks/${route.params.id}`
+            const taskRes = await axios.get(url)
+            task.value = taskRes.data
+        })
+
+        return {
+            task,
+        }
     },
 }
 </script>
