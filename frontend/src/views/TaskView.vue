1da44061e1f4978748cac75260f6a827fd41f48c
yuto14prog <yuto14prog@gmail.com>
2024-05-31 03:06:24 +0000

    setupの記法を修正

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -6,26 +6,17 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router'
 
-export default {
-    name: 'TaskView',
-    setup() {
-        const route = useRoute()
-        const task = ref({})
+const route = useRoute()
+const task = ref({})
 
-        onMounted(async function() {
-            const url = `/api/tasks/${route.params.id}`
-            const taskRes = await axios.get(url)
-            task.value = taskRes.data[0]
-        })
-
-        return {
-            task,
-        }
-    },
-}
+onMounted(async function() {
+    const url = `/api/tasks/${route.params.id}`
+    const taskRes = await axios.get(url)
+    task.value = taskRes.data[0]
+})
 </script>
