3141b2850340a4ddd91dd7418554da9462fa87b0
yuto14prog <yuto14prog@gmail.com>
2024-05-31 03:37:39 +0000

    Api取得といったん表示まで

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -3,6 +3,12 @@
         <h2 v-if="task.team">{{ task.team.name }} / {{ task.title }}</h2>
         <h3>内容</h3>
         <p>{{ task.body }}</p>
+
+        <h2>コメント</h2>
+        <div v-for="comment in comments" :key="comment.id">
+            <p>{{ comment.message }}</p>
+            <p>{{ comment.created_at }} by {{ comment.user.name }}</p>
+        </div>
     </div>
 </template>
 
@@ -13,10 +19,18 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const task = ref({})
+const comments = ref({})
 
 onMounted(async function() {
-    const url = `/api/tasks/${route.params.id}`
-    const taskRes = await axios.get(url)
-    task.value = taskRes.data[0]
+    try {
+        const [taskRes, CommentsRes] = await Promise.all([
+            axios.get(`/api/tasks/${route.params.id}`),
+            axios.get(`/api/tasks/${route.params.id}/comments`),
+        ])
+        task.value = taskRes.data[0]
+        comments.value = CommentsRes.data
+    } catch (err) {
+        console.log(err); // エラー処理、ページの状態管理は後で
+    }
 })
 </script>
