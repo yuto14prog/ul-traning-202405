ab5c356604f9c9e7807227d200b04c6762d61bdc
yuto14prog <yuto14prog@gmail.com>
2024-05-31 07:57:14 +0000

    Validationエラーを実装

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -13,6 +13,7 @@
         <span>本文</span>
         <form @submit.prevent>
             <textarea rows="5" v-model="message"></textarea>
+            <span v-if="errorMessage" class="error">{{ errorMessage }}</span><br>
             <label for="kind">完了報告とする</label>
             <input type="checkbox" id="kind" v-model="kind"><br>
             <button type="submit" @click="submit">送信</button>
@@ -27,6 +28,9 @@
 textarea {
     width: 100%;
 }
+.error {
+    color: red;
+}
 </style>
 
 <script setup>
@@ -39,6 +43,7 @@ const task = ref({})
 const comments = ref({})
 const message = ref('')
 const kind = ref(0)
+const errorMessage = ref()
 
 onMounted(async function() {
     try {
@@ -54,10 +59,19 @@ onMounted(async function() {
 })
 
 const submit = async function() {
-    await axios.post(`/api/tasks/${route.params.id}/comments`, {
-        message: message.value,
-        kind: kind.value,
-    }) // エラー処理する
+    try {
+        await axios.post(`/api/tasks/${route.params.id}/comments`, {
+            message: message.value,
+            kind: kind.value,
+        })
+        
+    } catch (err) {
+        if (axios.isAxiosError(err) && err.response.status == 422) {
+            errorMessage.value = '本文は必須です'
+        } else {
+            errorMessage.value = '予期せぬエラーです'
+        }
+    }
 }
 </script>
 
