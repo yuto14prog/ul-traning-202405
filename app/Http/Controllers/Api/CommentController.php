505621aa6f3426b7a8ab52c3ffde0939da590e9f
yuto14prog <yuto14prog@gmail.com>
2024-05-31 03:06:43 +0000

    Commentを返すように

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function index()
+    public function index(Task $task)
     {
-
+        return response()->json($task->comments()->with('user')->get());
     }
 
     public function store()
     {
-        
+
     }
 }
