ebc79287053a8ea421daf2f2ea22d100e5f24508
yuto14prog <yuto14prog@gmail.com>
2024-06-04 11:34:08 +0900

    Review:: 不要なカンマを削除

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,7 +11,7 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['message', 'kind',];
+    protected $fillable = ['message', 'kind'];
 
     public function task()
     {
